2b508fcee9f27538e35464258b7303d159426683
Ethan Jon <e10jc@users.noreply.github.com>
2018-04-22 12:41:11 -0700

    validating shopify requests

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,100 @@
+import {Agent} from 'https'
 import * as Koa from 'koa'
+import * as logger from 'koa-logger'
+import * as Router from 'koa-router'
+import * as fetch from 'node-fetch'
+import {URLSearchParams} from 'url'
+
+import validateRequestSignature from './validate-request-signature'
+
+const router = new Router()
+
+router.get('/', ctx => {
+  const {
+    hmac,
+    path_prefix: pathPrefix,
+    timestamp, 
+    signature,
+    shop
+  } = ctx.query
+
+  if (pathPrefix) {
+    ctx.body = `
+  <div>
+    <h1>Hey!</h1>
+    {{ collection }}
+    <script>
+      console.log(theme)
+    </script>
+  </div>`
+    ctx.type = 'application/liquid'
+    return
+  }
+
+  ctx.body = 'Hello'
+})
+
+router.get('/install', ctx => {
+  const {
+    scope, 
+    shop
+  } = ctx.query
+
+  if (!shop) {
+    ctx.body = 'Missing ?shop='
+    return
+  }
+
+  if (!scope) {
+    ctx.body = 'Missing ?scope='
+    return
+  }
+
+  const {
+    API_KEY, 
+    NONCE, 
+    REDIRECT_URI
+  } = process.env
+
+  ctx.redirect(`https://${shop}.myshopify.com/admin/oauth/authorize?client_id=${API_KEY}&amp;scope=${scope}&amp;redirect_uri=${REDIRECT_URI}&amp;state=${NONCE}`)
+})
+
+router.get('/install/confirm', async ctx => {
+  const {
+    code, 
+    hmac, 
+    timestamp, 
+    state, 
+    shop
+  } = ctx.query
+
+  const {
+    API_KEY, 
+    API_SECRET_KEY
+  } = process.env
+
+  const body = new URLSearchParams()
+  body.append('client_id', API_KEY)
+  body.append('client_secret', API_SECRET_KEY)
+  body.append('code', code)
+
+  const options = {
+    agent: new Agent({rejectUnauthorized: false}), 
+    body, 
+    method: 'POST'
+  }
+  const res = await fetch(`https://${shop}/admin/oauth/access_token`, options)
+  const {access_token: accessToken} = await res.json()
+
+  ctx.body = accessToken
+})
 
 const app = new Koa()
 
-app.use(async ctx => {
-  ctx.body = 'Hello World'
-})
+app
+  .use(logger())
+  .use(validateRequestSignature())
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 app.listen(3000)
---
+++ src/validate-request-signature.ts
@@ -0,0 +1,23 @@
+import * as crypto from 'crypto'
+
+export default () => async (ctx, next) => {
+  const {hmac, signature} = ctx.query
+  const {API_SECRET_KEY} = process.env
+
+  const message = Object.keys(ctx.query)
+    .filter(k => k !== 'signature' && k !== 'hmac')
+    .map(k => `${k}=${ctx.query[k]}`)
+    .sort()
+    .join(hmac ? '&' : '')
+
+  const digest = crypto
+    .createHmac('SHA256', API_SECRET_KEY)
+    .update(message)
+    .digest('hex')
+
+  if (digest !== signature && digest !== hmac) {
+    return ctx.throw(400)
+  }
+
+  await next()
+}
