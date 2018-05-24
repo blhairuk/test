ad36c94d7c98d203d414f027ac517431fb71cb06
Ethan Jon <e10jc@users.noreply.github.com>
2018-05-24 16:05:39 -0700

    removed video hero container

--- src/client/bundle-editor/components/choose-products/video-hero.tsx
+++ src/client/bundle-editor/components/choose-products/video-hero.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components"
 
 import Button from "../styled/button"
 import FlexWrapper from "../styled/flex-wrapper"
-import VideoHeroContainer from "../styled/video-hero-container"
 
 interface Props {
   openVideoModal: (youtubeId: string) => () => any,
@@ -20,9 +19,7 @@ export default class VideoHero extends React.Component<Props> {
     } = this.props
 
     return (
-      <VideoHeroContainer
-        imageUrl={`https://img.youtube.com/vi/${youtubeId}/hqdefault.jpg`}
-      >
+      <Wrapper imageUrl={`https://img.youtube.com/vi/${youtubeId}/hqdefault.jpg`}>
         <div className="text-center">
           <h1
             style={{
@@ -44,7 +41,7 @@ export default class VideoHero extends React.Component<Props> {
             </FlexWrapper>
           </Button>
         </div>
-      </VideoHeroContainer>
+      </Wrapper>
     )
   }
 }
@@ -52,4 +49,36 @@ export default class VideoHero extends React.Component<Props> {
 const ArrowContainer = styled.span`
   margin-left: 6px;
   transform: scale(2);
+`
+
+const borderRadius = "8px"
+
+interface WrapperProps {
+  imageUrl: string,
+}
+
+const Wrapper = styled.div`
+  align-items: center;
+  background-image: url(${({imageUrl}: WrapperProps) => imageUrl});
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius: ${borderRadius};
+  display: flex;
+  height: 200px;
+  position: relative;
+  justify-content: center;
+  z-index: 1;
+
+  &::after {
+    background: rgba(0, 0, 0, 0.45);
+    border-radius: ${borderRadius};
+    bottom: 0;
+    content: "";
+    left: 0;
+    position: absolute;
+    right: 0;
+    top: 0;
+    z-index: -1;
+  }
 `
