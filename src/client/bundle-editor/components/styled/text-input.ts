c771feb2e52e436a1f2d81f94e6ff1213e7bfb61
Ethan Jon <e10jc@users.noreply.github.com>
2018-05-31 10:09:00 -0700

    TextInput styling

--- src/client/bundle-editor/components/styled/text-input.ts
+++ src/client/bundle-editor/components/styled/text-input.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 
 export default styled.input`
+  background: none;
   border-bottom: 1px solid rgba(255, 255, 255, 0.25);
   border-top: 1px solid transparent;
   border-left: 1px solid transparent;
@@ -12,11 +13,13 @@ export default styled.input`
   transition: border-color 250ms ease-out;
 
   &:focus {
+    background: none;
     box-shadow: none;
     border-bottom: 1px solid #fff;
     border-top: 1px solid transparent;
     border-left: 1px solid transparent;
     border-right: 1px solid transparent;
+    color: #fff;
     outline-style: none;
   }
 `
