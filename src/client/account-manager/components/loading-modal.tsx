11c16c96cb6653079664fbfed7940466e6df62ff
Ethan Jon <e10jc@users.noreply.github.com>
2018-07-19 12:26:14 -0700

    bodymovin loading modal

--- src/client/account-manager/components/loading-modal.tsx
+++ src/client/account-manager/components/loading-modal.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import {Box, Text} from "rebass"
 import styled from "styled-components"
 
 import updateStateKeys from "../../helpers/update-state-keys"
@@ -16,6 +17,7 @@ export default class LoadingModal extends React.Component<{}, State> {
 
   public componentDidMount() {
     this.urgencyInterval = setInterval(this.handleUrgencyInterval, 1000)
+    this.initBodymovinAnimation()
   }
 
   public componentWillUnmount() {
@@ -27,7 +29,7 @@ export default class LoadingModal extends React.Component<{}, State> {
 
     return (
       <Wrapper>
-        <div>
+        <Text pt={2}>
           {(() => {
             if (numSecondsOnScreen < 5) {
               return "Loading..."
@@ -37,7 +39,8 @@ export default class LoadingModal extends React.Component<{}, State> {
               return "Sorry, still loading! Thanks for waiting..."
             }
           })()}
-        </div>
+        </Text>
+        <Box id="preloader" mx="auto" width="200px" />
       </Wrapper>
     )
   }
@@ -45,13 +48,28 @@ export default class LoadingModal extends React.Component<{}, State> {
   private handleUrgencyInterval = () => {
     this.setState(updateStateKeys({numSecondsOnScreen: this.state.numSecondsOnScreen + 1}))
   }
+
+  private initBodymovinAnimation = () => {
+    if (window.bodymovin) {
+      window.bodymovin.loadAnimation({
+        autoplay: true,
+        container: document.getElementById("preloader"),
+        loop: true,
+        name: "Build your box",
+        path: window.HH.AssetURLs.Preloader,
+        renderer: "svg",
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice",
+        },
+      })
+    }
+  }
 }
 
 const Wrapper = styled.div`
   background-color: #fff;
   border-radius: 5px;
   color: #000;
-  padding-bottom: 10px;
   position: relative;
   text-align: center;
 `
